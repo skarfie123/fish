650b1cea6f594b9b95da5f5cf5997004a047db0f
skarfie123 <rahulpai@rocketmail.com>
2024-04-22 11:28:03 +0100

    fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-cp -r . ~/.config/fish/conf.d
-cp -r . ~/.config/fish/functions
+cp -r conf.d ~/.config/fish/conf.d
+cp -r functions ~/.config/fish/functions
 cp ./config.fish ~/.config/fish/config.fish
 cp ./fish_variables ~/.config/fish/fish_variables
