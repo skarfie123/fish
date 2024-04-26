d0b46a81da2db56d5b67662a836e0d0a289e689b
skarfie123 <rahulpai@rocketmail.com>
2024-04-26 19:45:37 +0100

    don't back up fish_variables

--- install.sh
+++ install.sh
@@ -7,4 +7,3 @@ echo "Installing fish config"
 cp -r conf.d ~/.config/fish
 cp -r functions ~/.config/fish
 cp ./config.fish ~/.config/fish/config.fish
-cp ./fish_variables ~/.config/fish/fish_variables
