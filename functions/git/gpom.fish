e2f7de064e38eec5acc2f34bfbce8334505a67fe
skarfie123 <rahulpai@rocketmail.com>
2024-05-13 09:48:19 +0100

    fix gpom

--- functions/git/gpom.fish
+++ functions/git/gpom.fish
@@ -1,4 +1,4 @@
-function gpom --wraps='git pull origin master' --description 'alias gpom=git pull origin master'
-  git pull origin master $argv
+function gpom --wraps='git pull origin $(git config init.defaultBranch)' --description 'alias gpom=git pull origin $(git config init.defaultBranch)'
+  git pull origin $(git config init.defaultBranch) $argv
         
 end
