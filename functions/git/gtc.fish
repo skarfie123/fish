cf3b1af3707537a0521c0575ec37553271081214
skarfie123 <rahulpai@rocketmail.com>
2024-05-13 09:51:14 +0100

    gtc

---
+++ functions/git/gtc.fish
@@ -0,0 +1,4 @@
+function gtc --wraps='git town continue' --description 'alias gtc git town continue'
+  git town continue $argv
+        
+end
--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ set -eu
 echo "Installing fish config"
 
 cp -r conf.d ~/.config/fish
+if [ -d "~/.config/fish/functions" ]; then
+    rm ~/.config/fish/functions
+fi
 cp -r functions ~/.config/fish
 cp ./config.fish ~/.config/fish/config.fish
