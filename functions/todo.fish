cb70282c931b0bf00ed436ddf1ff281bca1fb8e4
skarfie123 <rahulpai@rocketmail.com>
2024-10-02 15:04:31 +0100

    todo

--- functions/todo.fish
+++ functions/todo.fish
@@ -1,4 +1,10 @@
-function todo --wraps=grep\ -rnEi\ --colour=always\ \ \"\#\ \\\?\|\#\ TODO\|\#\ FIXME\|\#\ HACK\|\\\[\ \\\]\|\\\[x\\\]\" --wraps=/usr/bin/grep\ -rnEi\ --colour=always\ \ \"//\ \\\?\|//\ TODO\|//\ FIXME\|//\ HACK\|\\\[\ \\\]\|\\\[x\\\]\"\ \*\*.cs\ \*\*.cshtml --description alias\ todo=/usr/bin/grep\ -rnEi\ --colour=always\ \ \"//\ \\\?\|//\ TODO\|//\ FIXME\|//\ HACK\|\\\[\ \\\]\|\\\[x\\\]\"\ \*\*.cs\ \*\*.cshtml
-  /usr/bin/grep -rnEi --colour=always  "// \?|// TODO|// FIXME|// HACK|\[ \]|\[x\]" **.cs **.cshtml $argv
-        
+function todo --description 'todo cs cshtml'
+  if count $argv > /dev/null
+    for i in $argv
+      echo $i
+      /usr/bin/grep -rnEi --colour=always  "// \?|// TODO|// FIXME|// HACK|# \?|# TODO|# FIXME|# HACK|\[ \]|\[x\]" **.$i
+    end
+  else
+    /usr/bin/grep -rnEi --colour=always  "// \?|// TODO|// FIXME|// HACK|# \?|# TODO|# FIXME|# HACK|\[ \]|\[x\]" **.*
+  end
 end
