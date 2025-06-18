01216387d395fa1d0b610ceac9d76371afa94234
skarfie123 <rahulpai@rocketmail.com>
2025-06-18 11:40:24 +0100

    drw

---
+++ functions/drw.fish
@@ -0,0 +1,4 @@
+function drw --wraps='dotnet run --property WarningLevel=0' --description 'alias drw dotnet run --property WarningLevel=0'
+  dotnet run --property WarningLevel=0 $argv
+        
+end
