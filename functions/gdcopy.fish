44deffd6828f3d295397343777285fdbb0e3ffa9
skarfie123 <rahulpai@rocketmail.com>
2025-08-14 17:57:29 +0100

    git diff details

--- functions/gdcopy.fish
+++ functions/gdcopy.fish
@@ -1,7 +1,13 @@
 function gdcopy --wraps=\\\(gd\\\)\ \|\ pbcopy
     begin
-        echo '```diff'
+        echo '<details>
+
+<summary>Git diff</summary>
+
+```diff'
         gd
-        echo '```'
+        echo '```
+
+</details>'
     end | pbcopy
 end
